36a5ab9622c1038119d5a59d05a86c4ce3b6639c
Melodt8tw <45555064+Melodt8tw@users.noreply.github.com>
2018-12-10 17:52:42 +0800

    Update bounceA12.10.ino

--- bounceA12.10/bounceA12.10.ino
+++ bounceA12.10/bounceA12.10.ino
@@ -1,11 +1,4 @@
 
-/* 
- DESCRIPTION
- ====================
- Simple example of the Bounce library that switches the debug LED when a button is pressed.
- */
-// Include the Bounce2 library found here :
-// https://github.com/thomasfredericks/Bounce2
 #include <Bounce2.h>
 
 #define rotate 0
@@ -14,32 +7,21 @@
 #define BUTTON_PIN 2
 #define LED_PIN 13
 
-// Instantiate a Bounce object
+
 Bounce debouncer = Bounce(); 
 
 void setup() {
 
    initAll();
-    pinMode(BUTTON_PIN,INPUT_PULLUP);
-
-  // After setting up the button, setup the Bounce instance :
-  debouncer.attach(BUTTON_PIN);
-  debouncer.interval(5); // interval in ms
-
-  //Setup the LED :
-  pinMode(LED_PIN,OUTPUT);
-
-
 }
 
 void loop() {
-  // Update the Bounce instance :
+ 
   debouncer.update();
 
-  // Get the updated value :
   int value = debouncer.read();
 
-  // Turn on or off the LED as determined by the state :
+  
   if ( value == LOW ) {
     digitalWrite(LED_PIN, HIGH );
     bell(GetRotate());
@@ -54,13 +36,20 @@ void initAll()
 {
   pinMode(buzzer,OUTPUT);
   digitalWrite(buzzer,LOW);
-  
+  pinMode(BUTTON_PIN,INPUT_PULLUP);
+
+ 
+  debouncer.attach(BUTTON_PIN);
+  debouncer.interval(5); 
+
+
+  pinMode(LED_PIN,OUTPUT);
 }
 
 void bell(double x)
 {
   digitalWrite(buzzer,HIGH);
-  delay(x = 500);
+  delay(x*1000);
   digitalWrite(buzzer,LOW);
 }
 
