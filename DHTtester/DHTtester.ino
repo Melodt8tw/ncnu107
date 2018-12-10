acbcac08703b25951d322326b0e76586c61700f5
Melodt8tw <45555064+Melodt8tw@users.noreply.github.com>
2018-12-10 17:58:54 +0800

    Update DHTtester.ino

--- DHTtester/DHTtester.ino
+++ DHTtester/DHTtester.ino
@@ -1,32 +1,22 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
 
 #include "DHT.h"
 
-#define DHTPIN 4     // what digital pin we're connected to
+#define DHTPIN 4    
+#define ledpin 13   
+#define ledpin2 12 
 
-// Uncomment whatever type you're using!
 #define DHTTYPE DHT11   // DHT 11
 //#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("DHTxx test!");
-
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
   dht.begin();
 }
 
@@ -66,4 +56,14 @@ void loop() {
   Serial.print(" *C ");
   Serial.print(hif);
   Serial.println(" *F");
+
+  if(t <= 28)
+  {digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+  }
+
+  if (t > 28)
+  {digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  }
 }
